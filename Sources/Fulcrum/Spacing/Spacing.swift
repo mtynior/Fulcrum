99423ab663c06d3e542e6e72afa457f1ca7bfc2e
Michal Tynior <michal.git@tynior.com>
2023-02-02 20:23:41 +0100

    Add spacing

---
+++ Sources/Fulcrum/Spacing/Spacing.swift
@@ -0,0 +1,47 @@
+//
+//  Spacing.swift
+//  Fulcrum
+//
+//  Created by Michal on 02/02/2023.
+//
+
+import UIKit
+
+public enum Spacing {
+    /// 0pt
+    public static let zero: CGFloat = 0
+    /// 2pt
+    public static let mini: CGFloat = 2
+    /// 4pt
+    public static let extraSmall: CGFloat = 4
+    /// 6pt
+    public static let small: CGFloat = 6
+    /// 8pt
+    public static let medium: CGFloat = 8
+    /// 10pt
+    public static let standard: CGFloat = 10
+    /// 14 pt
+    public static let largish: CGFloat = 14
+    /// 16pt
+    public static let large: CGFloat = 16
+    /// 20pt
+    public static let extraLarge: CGFloat =  20
+    /// 24pt
+    public static let extraLarge2: CGFloat = 24
+    /// 30pt
+    public static let extraLarge3: CGFloat = 30
+    /// 36pt
+    public static let extraLarge4: CGFloat = 36
+    /// 48pt
+    public static let extraLarge5: CGFloat = 48
+    /// 60pt
+    public static let extraLarge6: CGFloat = 60
+    /// 72pt
+    public static let extraLarge7: CGFloat = 72
+    /// 96pt
+    public static let extraLarge8: CGFloat = 96
+    /// 128pt
+    public static let extraLarge9: CGFloat = 128
+}
+
+public typealias CornerRadius = Spacing
