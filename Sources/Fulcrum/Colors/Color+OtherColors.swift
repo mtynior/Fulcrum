23156ffe124da1ef9a0eafe44d353fbc8dab9425
Michal Tynior <michal.git@tynior.com>
2023-02-04 13:10:01 +0100

    Add Other colors

---
+++ Sources/Fulcrum/Colors/Color+OtherColors.swift
@@ -0,0 +1,13 @@
+//
+//  Color+OtherColors.swift
+//  Fulcrum
+//
+//  Created by Michal on 04/02/2023.
+//
+
+import SwiftUI
+
+public extension Color {
+    /// A white color with opacity set to 0.001.
+    static let almostClear = Color.white.opacity(0.001)
+}
